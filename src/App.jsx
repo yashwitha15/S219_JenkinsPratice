79236d0c1c43b0ccd61aec9101a0d1976fd04df0
yashwitha15 <yashwithakasaneni15@gmail.com>
2025-08-04 10:39:37 +0530

    Update App.jsx
    Updated Jenkins base line

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecomJenkinsPratice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
